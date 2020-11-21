efc2820930a49655b471724dc77eef6c2cbd73c7
flokoe <koehler.florian@gmx.de>
2020-11-21 11:56:19 +0100

    Save request data in file

--- hooked.py
+++ hooked.py
@@ -6,19 +6,31 @@ A simple, general purpose webhook service which executes local commands.
 License: MIT (see LICENSE for details)
 """
 
-from bottle import post, run
-import subprocess
+from bottle import error, post, request, run
+import subprocess, json
 
 __author__ = 'Florian Köhler'
 __version__ = '0.0.1'
 __license__ = 'MIT'
 
 def exec_command():
-    command = ['echo', 'hello', 'world']
+    command = ['id']
     subprocess.run(command)
+
+@error(404)
+def error404(error):
+    return 'Pretty empty, huh...'
 
 @post('/payload')
 def payload():
-    exec_command()
+    f = open("request_data.txt", "w")
+
+    for item in request.headers:
+        f.write(f"{item}: {request.headers.get(item)}\n")
+
+    f.write("\n")
+    f.write(json.dumps(request.json, indent=4, sort_keys=True))
+    f.write("\n\n")
+    f.close()
 
 run(host='localhost', port=8090, reloader=True)
